c6603ec74c10eeccecf5555c5091b909f4578763
gloriaMoroyoqui <gloriamoroyoqui8121@gmail.com>
2026-05-18 19:22:01 -0700

    hice cambios

--- marketplace_main/store/models.py
+++ marketplace_main/store/models.py
@@ -76,6 +76,11 @@ class Cart(models.Model):
     def __str__(self):
         return f"Cart {self.id} - {self.user}"
 
+    # Actualizar
+    @property
+    def total(self):
+        return sum(item.subtotal for item in self.cartitem_set.all())
+
 
 # =========================
 # 🧾 CartItem (tabla intermedia)
@@ -90,9 +95,6 @@ class CartItem(models.Model):
 
     class Meta:
         unique_together = ('cart', 'product')
-
-    def __str__(self):
-        return f"{self.product} x {self.quantity}"
 
     # Actualizar
     @property
